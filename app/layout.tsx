4811003199935a2cd551d5af3493d545edd71931
Fuqiao Xue <xfq@w3.org>
2025-04-27 12:21:30 +0800

    Add Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,18 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <GoogleAnalyticsScript />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "raisiux4jm");
+            `
+          }}
+        />
       </head>
       <body className={cn("min-h-screen bg-gray-950 font-sans antialiased", fontSans.variable)}>
         <Suspense fallback={null}>
